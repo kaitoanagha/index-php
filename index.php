fb1eb3f5ea5206aa28896a2c47297b7ade072146
kaitoanagha <151990821+kaitoanagha@users.noreply.github.com>
2024-11-30 16:58:16 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Profil Saya</title>
+</head>
+<body>
+    <h1>Profil</h1>
+    <?php
+    // Data profil
+    $nama = "I Wayan Pasek Anagha Vinaya";
+    $nim = "23083000009";
+    $fakultas = "Teknologi Informasi";
+
+    // Tampilkan data profil
+    echo "<p>Nama: $nama</p>";
+    echo "<p>NIM: $nim</p>";
+    echo "<p>Fakultas: $fakultas</p>";
+    ?>
+</body>
+</html>
